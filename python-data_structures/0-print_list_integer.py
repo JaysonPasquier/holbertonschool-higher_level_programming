89c57b9f339590b9502ef3bc4d646de8ddbe8f66
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2025-01-20 13:18:03 +0100

    changed the task 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-	print('\n'.join(str(i) for i in my_list))
-	if my_list == []:
-		return None
-	return None
+	for i in my_list:
+		print("{:d}".format(i))
