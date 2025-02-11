781546229d574a1ebca68724399c847a8a410229
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2025-02-11 17:18:37 +0100

    change script

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """Read a text file (UTF8) and print it to stdout."""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
